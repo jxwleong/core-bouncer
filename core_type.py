1f4bf29ccbd04cae5bfbebbbb554ce5f65f8fee3
jxwleong <jason.xie.wei.leong@intel.com>
2022-09-05 04:45:08 +0000

    Compile linux excecutale for core_type

--- core_type.py
+++ core_type.py
@@ -1,6 +1,25 @@
+# Contact: jxwleong/xleong
 import cpuid
+import psutil
 
-cpu = cpuid.CPUID()
-regs = cpu(0x1a, 0)
-core_type = "Atom" if hex(regs[0] >> 24) == "0x20" else "Core" if hex(regs[0] >> 24) == "0x40" else f"Unknown type ({hex(regs[0] >> 24)})"
-print(core_type)
+
+def get_core_type():
+    cpu = cpuid.CPUID()
+    regs = cpu(0x1a, 0)
+    core_type = "Atom" if hex(regs[0] >> 24) == "0x20" else "Core" if hex(regs[0] >> 24) == "0x40" else f"Unknown type ({hex(regs[0] >> 24)})"
+    return core_type
+
+total_core_count = psutil.cpu_count()
+
+
+def main():
+    for core in range(total_core_count):
+        current_process = psutil.Process()
+    
+        # Set affinity
+        current_process.cpu_affinity([core])  # expecting list terable
+        print(f"Core{core}------------->{get_core_type()}")
+
+
+if __name__ == "__main__":
+    main()
