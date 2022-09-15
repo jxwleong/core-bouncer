41970749535bc4cf6de65bc2a771dd3a0050968d
Leong, Jason Xie Wei <jason.xie.wei.leong@intel.com>
2022-09-15 16:15:02 +0800

    Fixed typo for command as for windows it is using backslash

--- core_bouncer.py
+++ core_bouncer.py
@@ -3,7 +3,17 @@ import subprocess
 import json
 import os
 
-command = "bin/linux/core_type" if os.name == "posix" else "bin/windows/core_type.exe" if os.name == "nt" else "Not supported OS"
-core_mapping_process = subprocess.run(command, capture_output=True)
+ROOT = os.path.normpath(os.path.join(os.path.abspath(__file__), ".."))
+command = "bin/linux/core_type" if os.name == "posix" else "bin\windows\core_type.exe" if os.name == "nt" else "Not supported OS"
+
+# Make full path to the binaries instead of relative path
+full_command = os.path.join(ROOT, command)
+core_mapping_process = subprocess.run(full_command, capture_output=True)
 core_mapping_dict = json.loads(core_mapping_process.stdout.decode("utf-8"))
+
+if 'Unknown type (0x0)' in core_mapping_dict.values():      
+    print("Not Hybrid Core Config detected!")
+else:
+    print("Hybrid Core detected!")
+
 print(core_mapping_dict)
